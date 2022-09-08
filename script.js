882bfd23c0d6cda19057a6effd4894ae62cd7072
SKustassoo <44232116+SKustassoo@users.noreply.github.com>
2022-09-08 12:08:51 +0300

    update draw size
    added a functioning drawing canvas block size selector.
    - clears previous canvas
    - creates new one

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let currentColor = "black"
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "black";
+
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+let drawCanvas = document.getElementById("main-content");
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
 
 //set default draw area 16 x 16
 //populate draw area with divs
-function drawTheArea(drawMesures = 16) {
+function drawTheArea(drawMesures) {
 
     var grid = document.createElement('div'); // create a grid
     grid.className = 'draw-area';
@@ -16,25 +28,43 @@ function drawTheArea(drawMesures = 16) {
             rowElement.className = 'row';
             rowElement.id = i+""+j;
   
-            rowElement.addEventListener('click', function () {drawBox(this.id), currentColor});
+            rowElement.addEventListener('mouseover', drawBox);
+            rowElement.addEventListener('mousedown', drawBox);
 
             column.appendChild(rowElement); // append row in column
         }
 
         grid.appendChild(column); // append column inside grid
     }
-    document.getElementById("main-content").appendChild(grid);
-}
+    drawCanvas.appendChild(grid);
+};
 
 
-function drawBox(inputId, inputColor = "black"){
-    document.getElementById(inputId).style.backgroundColor = inputColor;
-}
+function drawBox(e){
+    if (e.type === 'mouseover' && mouseDown){
+        e.target.style.backgroundColor = currentColor
+    };
+};
 
 
 
+function resetCanvas() {
+    console.log("script was triggered");
+    currentSize = document.getElementById("resize-form").elements[0].value;
 
-drawTheArea();
+    const refresher = document.getElementsByClassName('draw-area')[0].remove();
+    console.log(refresher);
+
+    console.log(currentSize);
+    drawTheArea(currentSize);
+
+    //document.getElementById("draw-area").innerHTML="";
+    //drawTheArea(currentSize);
+};
+
+//document.addEventListener('DOMContentLoaded', resetCanvas);
+
+window.onload = () => {drawTheArea(currentSize)};
 
 
 
